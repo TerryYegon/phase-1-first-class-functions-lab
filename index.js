e98850a4759a3a912cf57182c0d22d0df3f287a8
Terry Yegon <yegonterry618@gmail.com>
2025-06-06 19:21:38 +0300

    task completed

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2, 4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+console.log(selectingDrivers[o](drivers));
+console.log(selectingDrivers[1](drivers));
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+}
+
+function fareDoubler(fare) {
+return fare * 2;
+}
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+ 
+const returnFirstTwoDrivers = drivers => drivers.slice(0, 2);
+
+
+const returnLastTwoDrivers = drivers => drivers.slice(-2);
+
+
+const selectDifferentDrivers = (drivers, fn) => fn(drivers);
+
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers)); // ['Antonia', 'Nuru']
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));  // ['Amari', 'Mo']
